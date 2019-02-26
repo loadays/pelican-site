5b02fea1ddfbe5845a2d41a0f266cf7f904c8bb3
Toshaan Bharvani <toshaan@vantosh.com>
2019-02-26 12:11:11 +0100

    navbar tuple correction
    Signed-off-by: Toshaan Bharvani <toshaan@vantosh.com>

--- publishconf.py
+++ publishconf.py
@@ -25,7 +25,7 @@ MENUITEMS = (
             ('Home', 'http://loadays.org'),
             ('Venue','/pages/venue.html'),
 #            ('Schedule','/pages/schedule.html'),
-            ('CfP', 'https://cfp.loadays.org/2019/cfp')
+            ('CfP', 'https://cfp.loadays.org/2019/cfp'),
             ('Social','/pages/social.html'),
             ('FAQ','/pages/faq.html'),
             ('Sponsors','/pages/sponsors.html'),
